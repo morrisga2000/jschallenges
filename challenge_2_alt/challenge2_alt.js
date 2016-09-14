40d27d4caf227e4e4710311f4a1e0c583745a36e
Gregory Morris <morrisga2000@yahoo.com>
2016-09-14 11:07:59 -0400

    second commit

--- challenge_2_alt/challenge2_alt.js
+++ challenge_2_alt/challenge2_alt.js
@@ -33,27 +33,39 @@ var numOfShotsTeam1 = 0;
 var numOfShotsTeam2 = 0;
 var totalShots = 0;
 var num_resets = 0;
-var numOFHitsTeam1 = 0;
+var numOfHitsTeam1 = 0;
 var numOfHitsTeam2 = 0;
- var x = Math.random();
+ var x = 0;
+var y = 0;
 
-
+    var audio = $("audio")[0];
+    audio.play(); 
 
 //$(document).ready(function(){
  $("#teamone-shoot").click(function(){
-     $("body").css("background-color", "lightgray");
      numOfShotsTeam1++;
-     	 $("#teamone-numshots").replaceWith(numOfShotsTeam1);
-		numOFHitsTeam1 += Math.round(x);
+     console.log(numOfShotsTeam1);
+     	 $("#teamone-numshots").text(numOfShotsTeam1);
+     	 x = Math.random();
+		numOfHitsTeam1 += Math.round(x);
+		if (Math.round(x)== 1) {
+			     $("#teamone-numhits").text(numOfHitsTeam1);
+			     $("body").css("background-color", "lightgray");
+			 }
 });
 //});
 
 //$(document).ready(function(){
  $("#teamtwo-shoot").click(function(){
-     $("body").css("background-color", "red");
      numOfShotsTeam2++;
-     	 $("#teamtwo-numshots").replaceWith(numOfShotsTeam2);
-     	 numOFHitsTeam2 += Math.round(x);
+     	 $("#teamtwo-numshots").text(numOfShotsTeam2);
+     	 y = Math.random();
+     	 numOfHitsTeam2 += Math.round(y);
+		if (Math.round(y)== 1) {
+				 $("#teamtwo-numhits").text(numOfHitsTeam2);
+			     $("body").css("background-color", "red");
+			 }
+
 });
 //});
 
@@ -63,9 +75,41 @@ var numOfHitsTeam2 = 0;
      numOfShotsTeam1 = 0;
 	 numOfShotsTeam2 = 0;
 	 totalShots = 0;
+	 numOfHitsTeam1 = 0;
+	 numOfHitsTeam2 = 0;
 	 num_resets++;
-	 $("#num-resets").replaceWith(num_resets);
+	 $("#num-resets").text(num_resets);
+	      	 $("#teamone-numshots").text(numOfShotsTeam1);
+     	     $("#teamtwo-numshots").text(numOfShotsTeam2);
+			 $("#teamone-numhits").text(numOfHitsTeam1);
+			 $("#teamtwo-numhits").text(numOfHitsTeam2);
+
 							});
+
+
+
+
+    $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', 'Hockey_Stick_Slap.mp3');
+        audioElement.setAttribute('autoplay', 'autoplay');
+        //audioElement.load()
+
+        $.get();
+
+        audioElement.addEventListener("load", function() {
+            audioElement.play();
+        }, true);
+
+        $('.play').click(function() {
+            audioElement.play();
+        });
+
+        $('.pause').click(function() {
+            audioElement.pause();
+        });
+    }); 
+
  //});
 
  //$(function(){
